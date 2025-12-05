84d50dc7517c61d9716408a96a5bb53d7143c15c
Andrish97 <andrew.shum1997@gmail.com>
2025-12-05 19:36:39 +0100

    Create games-api.js

---
+++ js/core/games-api.js
@@ -0,0 +1,9 @@
+// js/core/games-api.js
+window.ArcadeGames = (() => {
+  // tutaj: fetch("games.json"), sb.from("games") itd.
+  // i na końcu:
+  return {
+    getGamesGroupedByCategory,
+    // itp...
+  };
+})();
